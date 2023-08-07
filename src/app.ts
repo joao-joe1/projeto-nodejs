cd540b81a56cb00753d938daaf40c9a804bbf6b2
Joe <joaokingteam@hotmail.com>
2023-08-07 16:04:01 -0300

    Erro corrigido.

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import 'express-async-errors';
+import express, { Request, Response, NextFunction } from "express";
+import { router } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        });
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+});
+
+export { app }
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,6 @@
-import 'express-async-errors';
-import express, { Request, Response, NextFunction } from "express";
-import { router } from "./routes";
+import { app } from './app'
 
-const app = express();
-
-app.use(express.json());
-
-app.use(router);
-
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof Error) {
-        return response.status(400).json({
-            error: err.message
-        });
-    }
-    return response.status(500).json({
-        status: "error",
-        message: "Internal Server Error"
-    });
-});
+// const app = require('./app');
 
 app.listen(3000, () => {
     console.log('Server is running in port 3000')
