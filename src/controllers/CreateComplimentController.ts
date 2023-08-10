ee8dce3ceb4b42c1b7212ac01723781c6b1b4702
Joe <joaokingteam@hotmail.com>
2023-08-10 16:07:21 -0300

    compliments

---
+++ src/controllers/CreateComplimentController.ts
@@ -0,0 +1,13 @@
+import { Response, Request } from "express"
+import { CreateComplimentService } from "../services/complimentsService"
+
+class CreateComplimentController {
+    async handle(request: Request, response: Response) {
+        const { tag_id, user_sender, user_receiver, message } = request.body
+        const createComplimentService = new CreateComplimentService();
+        const newCompliment = await createComplimentService.execute({ tag_id, user_sender, user_receiver, message })
+        return response.status(201).json({ newCompliment })
+    }
+}
+
+export { CreateComplimentController }
--- src/services/complimentsService.ts
+++ src/services/complimentsService.ts
@@ -1,3 +1,7 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
 interface InterfaceComplimentRequest {
     tag_id: string,
     user_sender: string,
@@ -9,6 +13,28 @@ interface InterfaceComplimentRequest {
 class CreateComplimentService {
     async execute({ tag_id, user_sender, user_receiver, message }: InterfaceComplimentRequest) {
 
+        if (user_receiver === user_sender) {
+            throw new Error("O remetente e o destinatário não podem ser os mesmos usuários.")
+        }
+
+        const userReceiverExists = await prisma.users.findUnique({
+            where: { id: user_receiver }
+        })
+
+        if (!userReceiverExists) {
+            throw new Error("O destinatário do elogio é inválido ou não existe.")
+        }
+
+        const compliment = await prisma.compliments.create({
+            data: {
+                tag_id,
+                user_sender,
+                user_receiver,
+                message
+            }
+        })
+
+        return compliment
     }
 }
 
