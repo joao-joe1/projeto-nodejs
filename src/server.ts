23b6ec75754b2a5c9821dc8f43a233533e088119
Joe <joaokingteam@hotmail.com>
2023-08-11 17:49:37 -0300

    attmessagelisten

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app'
 
 app.listen(3000, () => {
-    console.log('Server is running in port 3000')
+    console.log('Servidor em execução na porta 3000. Acesse http://localhost:3000 para começar a explorar sua aplicação.')
 })
